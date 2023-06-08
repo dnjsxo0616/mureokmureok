acf3d44b46bd911468e423a96f48c1c1c0bd9d0f
KDT live <dnjsxo0616@gmail.com>
2023-06-08 17:34:31 +0900

    ADD_sales_order

--- sales/models.py
+++ sales/models.py
@@ -5,7 +5,7 @@ from imagekit.processors import Thumbnail
 from django.utils import timezone
 from datetime import timedelta,datetime
 from django_ckeditor_5.fields import CKEditor5Field
-
+import random
 def sale_img_path(instance, filename):
     return f'images/sale/{instance.title}/{filename}'
 # Create your models here.
@@ -34,12 +34,22 @@ class Order(models.Model):
     product_name = models.CharField(max_length=50)
     payment_status = models.BooleanField(default=False)
     quantity = models.PositiveIntegerField()
-    price = models.DecimalField(max_digits=10, decimal_places=2)
+    price = models.PositiveIntegerField()
+    order_number = models.CharField(max_length=10, unique=True)
 
     def __str__(self):
         return f"Order {self.id}"
 
+    def save(self, *args, **kwargs):
+        if not self.order_number:
+            self.order_number = self.generate_order_number()
+        super().save(*args, **kwargs)
 
+    def generate_order_number(self):
+        length = 10
+        order_number = str(random.randint(0, 9999999999)).zfill(length)
+        return order_number
+    
 class Review(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='reviews')
