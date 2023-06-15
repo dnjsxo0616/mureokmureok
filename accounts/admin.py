194a45aec46d98fbf25f65bf51a63a1a1ab7e28f
illson97 <illson97@github.com>
2023-06-15 18:18:31 +0900

    수정-칭호

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import User, User_title
+from .models import User, User_title, User_profile
 
 # Register your models here.
 
 admin.site.register(User, UserAdmin)
 admin.site.register(User_title)
+admin.site.register(User_profile)
